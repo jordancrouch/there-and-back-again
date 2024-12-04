55c184aed33f3cfbbfd9950c1f6a4a2331e11a54
Jordan Crouch <107533237+jordancrouch@users.noreply.github.com>
2024-12-04 21:29:50 +0000

    Add js to show modal on form submission

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,14 +1,25 @@
 // Wait for page to finish loading
 window.addEventListener("load", () => {
   let grid = document.querySelector("#masonry-grid");
+  let form = document.getElementById("message-form");
+  const thankyouModal = new bootstrap.Modal(
+    document.getElementById("thankyou-modal")
+  );
 
-  // Inititialise Masonry
-  let msnry = new Masonry(grid, {
-    percentPosition: true,
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    thankyouModal.show();
   });
 
-  imagesLoaded(grid).on("progress", () => {
-    // Layout Masonry after each image loads
-    masonry.layout();
-  });
+  if (grid !== null) {
+    // Inititialise Masonry
+    let msnry = new Masonry(grid, {
+      percentPosition: true,
+    });
+
+    imagesLoaded(grid).on("progress", () => {
+      // Layout Masonry after each image loads
+      masonry.layout();
+    });
+  }
 });
